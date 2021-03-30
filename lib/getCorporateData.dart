79fb6c631c2023706cebb767b67c997c9dd8f43c
Vinicius Guidotti Moraes <vinicius@zapway.com.br>
2021-03-29 22:32:27 -0300

    Page 03 - Data Collect

--- lib/getCorporateData.dart
+++ lib/getCorporateData.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'constants.dart';
+
 class CorporateForm extends StatefulWidget {
   @override
   MyCustomFormState createState() {
@@ -7,24 +9,146 @@ class CorporateForm extends StatefulWidget {
   }
 }
 
-// Define a corresponding State class.
-// This class holds data related to the form.
 class MyCustomFormState extends State<CorporateForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a `GlobalKey<FormState>`,
-  // not a GlobalKey<MyCustomFormState>.
-  final _formKey = GlobalKey<FormState>();
-
+  var _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          // Add TextFormFields and ElevatedButton here.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LENC - Coleta de Dados'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 30,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("images/logo_invert.jpg"))),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: "Seu Nome",
+                    labelText: "Nome",
+                    hintStyle: TextStyle(color: Colors.white),
+                    labelStyle: TextStyle(color: Colors.yellow, fontSize: 24),
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person_pin_circle),
+                    fillColor: Colors.black12,
+                    filled: true,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: "Seu E-mail",
+                    labelText: "E-mail",
+                    hintStyle: TextStyle(color: Colors.white),
+                    labelStyle: TextStyle(color: Colors.yellow, fontSize: 24),
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                    fillColor: Colors.black12,
+                    filled: true,
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: "Seu Telefone",
+                    labelText: "Telefone",
+                    hintStyle: TextStyle(color: Colors.white),
+                    labelStyle: TextStyle(color: Colors.yellow, fontSize: 24),
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                    fillColor: Colors.black12,
+                    filled: true,
+                  ),
+                  keyboardType: TextInputType.phone,
+                ),
+                TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: "Nome da Empresa",
+                    labelText: "Empresa",
+                    hintStyle: TextStyle(color: Colors.white),
+                    labelStyle: TextStyle(color: Colors.yellow, fontSize: 24),
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.apartment),
+                    fillColor: Colors.black12,
+                    filled: true,
+                  ),
+                  keyboardType: TextInputType.name,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: "Setor da Empresa",
+                    labelText: "Setor Empresarial",
+                    hintStyle: TextStyle(color: Colors.white),
+                    labelStyle: TextStyle(color: Colors.yellow, fontSize: 24),
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.card_travel),
+                    fillColor: Colors.black12,
+                    filled: true,
+                  ),
+                  keyboardType: TextInputType.name,
+                ),
+                FittedBox(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return CorporateForm();
+                      }));
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(top: 15, bottom: 10),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 16),
+                      decoration: BoxDecoration(
+                          color: kPrimaryColor,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Salvar e Continuar",
+                            style: Theme.of(context)
+                                .textTheme
+                                .button
+                                .copyWith(color: Colors.black),
+                          ),
+                          Icon(Icons.arrow_forward)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+              ],
+            ),
+          ),
         ],
       ),
     );
